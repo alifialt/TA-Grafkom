982d25945f405085aae49ef6b3a67f1664201fd9
ZulfanMK <zulfanmizan@gmail.com>
2023-11-30 15:43:49 +0700

    level game

--- whack_a_mole.py
+++ whack_a_mole.py
@@ -6,15 +6,19 @@ import random
 from os import path
 from pygame.locals import *
 
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'img')
+audio_folder = os.path.join(game_folder, 'audio')
+
 pygame.init()
 
-SCREEN = pygame.display.set_mode((1280,720))
+SCREEN = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Menu')
 
-BG = pygame.image.load("img/land.png")
+BG = pygame.image.load(path.join(img_folder, 'land.png'))
 
 def get_font(size): # Returns Press-Start-2P in the desired size
-    return pygame.font.Font("img/Roboto-Regular.ttf", size)
+    return pygame.font.Font(path.join(img_folder, 'Roboto-Regular.ttf'))
 
 def play():
     pygame.display.set_caption("Play")
@@ -23,8 +27,6 @@ def play():
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
         SCREEN.fill("black")
-        game_folder = os.path.dirname(__file__)
-        img_folder = os.path.join(game_folder, 'img')
 
         WIDTH = 800
         HEIGHT = 600
@@ -32,7 +34,7 @@ def play():
 
         # RGB
         WHITE = (255, 255, 255)
-        BALCK = (0, 0, 0)
+        BLACK = (0, 0, 0)
         RED = (255, 0, 0)
         BLUE = (0, 0, 255)
         GREEN = (0, 255, 0)
@@ -62,6 +64,11 @@ def play():
             font_surface = font.render(text, True, font_color)
             screen.blit(font_surface, (x, y))
 
+        def set_level_speed(level):
+            speeds = [3000, 2000, 1000]
+            return speeds[level - 1] if 0 <= level - 1 < len(speeds) else speeds[0]
+
+
         pygame.init()
         pygame.mixer.init()
 
@@ -73,10 +80,14 @@ def play():
         mouse_pos = (0, 0)
         pygame.mouse.set_visible(False)
         countdown = 5
+        level_durations = [0,10, 10, 10]
+        current_level = 1
         last_update = pygame.time.get_ticks()
+        score = 0
+        last_mole_move = pygame.time.get_ticks()
 
         # load sound
-        bonk = pygame.mixer.Sound(path.join(img_folder, 'bonk.mp3'))
+        bonk = pygame.mixer.Sound(path.join(audio_folder, 'bonk.mp3'))
 
         # load images
         mole = pygame.transform.scale(pygame.image.load(path.join(img_folder, 'mice.png')), (70, 70))
@@ -87,48 +98,100 @@ def play():
 
         hammer = []
         for i in range(1, 3):
-            img = pygame.image.load(path.join(img_folder, 'hammer{}.png'.format(i))).convert_alpha
+            img = pygame.image.load(path.join(img_folder, 'hammer{}.png'.format(i))).convert_alpha()
             hammer.append(img)
 
         hammer_img = hammer[0]
-        hammer_surface = pygame.transform.scale(hammer_img(), (200, 200))
+        hammer_surface = pygame.transform.scale(hammer_img, (200, 200))
         hammer_rect = hammer_surface.get_rect()
+        draw_land()
+
+        exit_button = pygame.transform.scale(pygame.image.load(path.join(img_folder, 'exit_button.png')).convert_alpha(), (100, 50))
+        exit_button_rect = exit_button.get_rect()
+        exit_button_rect.center = (WIDTH // 2 - 9, HEIGHT // 2 + 130)
 
         run = True
+        show_score = False
+        game_over = False
+
         while run:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     run = False
                 if event.type == MOUSEBUTTONDOWN:
                     if event.button == 1:
-                        if mole_rect.collidepoint(mouse_pos):
+                        if mole_rect.collidepoint(mouse_pos) and not game_over:
                             bonk.play()
+                            score += 1
                         hammer_img = hammer[1]
-                        hammer_surface = pygame.transform.scale(hammer_img(), (200, 200))
-                        random_mole_position()
+                        hammer_surface = pygame.transform.scale(hammer_img, (200, 200))
+                        if not game_over:
+                            random_mole_position()
                 if event.type == MOUSEBUTTONUP:
                     if event.button == 1:
                         hammer_img = hammer[0]
-                        hammer_surface = pygame.transform.scale(hammer_img(), (200, 200)) 
-
+                        hammer_surface = pygame.transform.scale(hammer_img, (200, 200))
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        run = False
                 mouse_pos = pygame.mouse.get_pos()
                 hammer_rect.center = (mouse_pos[0], mouse_pos[1])
 
-            pygame.display.flip()
-
             now = pygame.time.get_ticks()
-            if now - last_update > 1000 and countdown > 0:
-                last_update = now
-                countdown -= 1
-                random_mole_position()
-
-            screen.fill(BG)
             if countdown > 0:
-                draw_text(str(countdown), 40, WHITE, WIDTH//2, 20)
+                if now - last_update > 1000:
+                    last_update = now
+                    countdown -= 1
+                    if not game_over:
+                        random_mole_position()
+                    screen.fill(BG)
+                    draw_text(str(countdown), 40, WHITE, WIDTH//2, 20)
+                else:
+                    screen.fill(BG)
+                    draw_text(str(countdown), 40, WHITE, WIDTH//2, 20)
             else:
+                if now - last_update < level_durations[current_level] * 1000:
+                    screen.fill(BG)
+                    draw_text(str(level_durations[current_level] - int((now - last_update) / 1000)), 40, WHITE, WIDTH//2, 20)
+                else:
+
+                    last_update = now
+                    countdown = 5
+                    current_level += 1
+                    if current_level == 1:
+                        level_duration = 10
+                    elif current_level == 2:
+                        level_duration = 15
+                    elif current_level == 3:
+                        level_duration = 20
+                    elif current_level == 4:
+                        game_over = True
+                        countdown = 1000
+            if now - last_mole_move > set_level_speed(current_level) and countdown == 0 and not game_over:
+                random_mole_position()
+                last_mole_move = now
+
+            if countdown == 0:
                 screen.blit(mole, mole_rect)
+                draw_land()
+
+            if game_over:
+                screen.fill(BG)
+                draw_text("Skor: {}".format(score), 40, WHITE, WIDTH//2 - 60, HEIGHT//2 - 60)
+                draw_text("klik EXIT untuk keluar", 30, BLACK, WIDTH//2 - 120, HEIGHT//2 )
+
+            # Gambar tombol keluar
+                screen.blit(exit_button, exit_button_rect)
+
+            # Periksa klik tombol keluar
+                if exit_button_rect.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]:
+                    run = False
+
             draw_land()
             screen.blit(hammer_surface, hammer_rect)
+            pygame.display.flip()
+            clock.tick(FPS)
+
 
         pygame.quit()
 
@@ -145,11 +208,11 @@ def main_menu():
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
         MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 70))
+        MENU_RECT = MENU_TEXT.get_rect(center=(400, 200))
 
-        PLAY_BUTTON = Button(image=None, pos=(640,250),
+        PLAY_BUTTON = Button(image=None, pos=(400,250),
                              text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="orange")
-        QUIT_BUTTON = Button(image=None, pos=(640,450),
+        QUIT_BUTTON = Button(image=None, pos=(400,300),
                              text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="orange")
         
         SCREEN.blit(MENU_TEXT, MENU_RECT)
